f8ca20ccffd85354e2d64a23ba8403b8dbbb3e8d
Lucas Souza <lukeeifsp@hotmail.com>
2019-03-11 00:10:12 -0300

    Update

--- src/reactionrole.js
+++ src/reactionrole.js
@@ -1,14 +1,18 @@
 module.exports = class ReactionRole {
     /**
-     * @param {Options} [options={}] Options for the reaction role
+     * @example 
+     *  {Client} Bot client.
+     *  {Guild} The guild for this Reaction Role
+     *  {Message} Message to ReactionRole
+     *  {Role} Role to given after react
+     *  {Emoji} Emoji to this role
      */
     constructor(options){
-        this.client = options.message.client
-        this.guild = options.message.guild
-        this.channel = options.message.channel
-        this.message = options.message
-        this.role = options.role
-        this.emoji = options.emoji
+        this.client = options.client ? options.client : options.message.client
+        this.guild = options.guild ? options.guild : options.message.guild
+        this.message = options.message.id ? options.message.id : options.message
+        this.role = options.role.id ? options.role.id : options.role
+        this.emoji = options.emoji.id ? options.emoji.id : options.emoji
         this.onlyadd = options.onlyadd == true ? true : false //optional
         this.debug = options.debug == true ? true : false //optional
         this.max = options.max > 0 ? options.max : Number.MAX_SAFE_INTEGER //optional
@@ -17,17 +21,19 @@ module.exports = class ReactionRole {
         this.load()
     }
     load(){
-        if(!this.client) return console.log('Client from message is undefined!')
-        if(!this.guild || typeof this.guild != 'object') return console.log('Guild from message is undefined!')
-        if(!this.channel || typeof this.channel != 'object') return console.log('Channel from message is undefined!')
-        this.role.id ? this.role = this.role.id : this.role
-        if(!this.role || isNaN(this.role)) return console.log('Role format invalid!')
-        if(!this.message || typeof this.message != 'object') return console.log('Message format invalid!')
-        if(isNaN(this.emoji)) this.emoji.id ? this.emoji = this.emoji.id : this.emoji = this.emoji.name
-        if(!this.emoji || isNaN(this.emoji)) return console.log('Emoji format invalid!')
-
+        if(!this.client || typeof this.client != 'object') return console.log('Client from message is undefined!')
+        else if(typeof this.guild != 'object') this.guild = this.client.guilds.get(this.guild)
+        else if(typeof this.guild != 'object' || !this.guild) return console.log('Guild format invalid!')
+        else if(typeof this.role == 'object') this.role.id ? this.role = this.role.id : this.role
+        else if(typeof this.role != 'number') return console.log('Role format invalid!')
+        else if(typeof this.message != 'object' && typeof this.message != 'number') return console.log('Message format invalid!')
+        else if(typeof this.emoji == 'object') this.emoji.id ? this.emoji = this.emoji.id : this.emoji = this.emoji.name
+        else if(typeof this.emoji != 'string' && typeof this.emoji != 'number') return console.log('Emoji format invalid!')
+        //Se está tudo certo, pode iniciar!
+        else {
         this.client.on('raw', evento => this.run(evento))
         if(this.debug) console.log(`[DEBUG] : ReactionRole ${this.role} started!`)
+        }
     }
     run(evento){
     if(evento.t != 'MESSAGE_REACTION_ADD' && evento.t != 'MESSAGE_REACTION_REMOVE') return
@@ -35,9 +41,9 @@ module.exports = class ReactionRole {
         if(this.onlyadd && !typeAdd || this.uses >= this.max) return
         evento = evento.d
         let emoji = evento.emoji.id ? evento.emoji.id : evento.emoji.name
-        if(this.message.id == evento.message_id && this.emoji == emoji){
+        if(this.message == evento.message_id && this.emoji == emoji){
             let membro = this.guild.members.get(evento.user_id)
-            if(!membro) return console.log(`I can't get guild ${evento.guild_id}!`)
+            if(!membro) return
             if(typeAdd) membro.addRole(this.role).then(() => {if(this.debug) console.log(`[DEBUG] : User ${evento.user_id} reacted with emoji ${this.emoji} and received the role ${this.role}`)}).catch(e => console.log(e))
             else membro.removeRole(this.role).then(() => {if(this.debug) console.log(`[DEBUG] : User ${evento.user_id} reacted with emoji ${this.emoji} and lost the role ${this.role}`)}).catch(e => console.log(e))
             this.uses += 1
